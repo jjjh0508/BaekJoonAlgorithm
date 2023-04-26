8c8779c29f1550d3c4e46f42fa1f4a80c39ca8b8
JIHWAN <jjjh0508@gmail.com>
2023-04-26 21:15:07 +0900

    4월26일

---
+++ Algorithm/src/baejoon/BaeJoon5522.java
@@ -0,0 +1,24 @@
+package baejoon;
+
+import java.util.Scanner;
+
+public class BaeJoon5522 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		byte[] card = new byte[5];
+		int sum=0;
+		for(int i=0;i<card.length;i++) {
+			byte bnum = sc.nextByte();
+			if(bnum>=0&&bnum<=100) {
+				card[i]=bnum;
+			}
+			
+		}
+		for(int i=0;i<card.length;i++) {
+			sum+=card[i];
+		}
+		System.out.println(sum);
+	}
+
+}
