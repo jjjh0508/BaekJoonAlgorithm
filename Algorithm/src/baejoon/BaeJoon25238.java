b5d67d8cc0a122869a9d597d81e78582bee146f7
JIHWAN <jjjh0508@gmail.com>
2023-04-27 21:09:34 +0900

    4월27일

---
+++ Algorithm/src/baejoon/BaeJoon25238.java
@@ -0,0 +1,24 @@
+package baejoon;
+
+import java.util.Scanner;
+/*
+ * (a*100)-(a*b)<10000)
+ *  b*0.01
+ */
+public class BaeJoon25238 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if((a*100)-(a*b)<10000) {
+			System.out.println(1);
+		
+		}else {
+			System.out.println(0);
+		}
+	}
+
+}
