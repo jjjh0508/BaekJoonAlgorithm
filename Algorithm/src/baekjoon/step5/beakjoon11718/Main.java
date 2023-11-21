b9f1ea6869af15e5cb030c38277b6567fc4f3ffb
JIHWAN <jjjh0508@gmail.com>
2023-11-21 18:43:19 +0900

    9086

---
+++ Algorithm/src/baekjoon/step5/beakjoon11718/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.step5.beakjoon11718;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+			Scanner sc =new Scanner(System.in);
+			String string = sc.nextLine();
+			System.out.println(string);
+			
+		
+	}
+
+}
--- Algorithm/src/baekjoon/step5/beakjoon9086/Main.java
+++ Algorithm/src/baekjoon/step5/beakjoon9086/Main.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-	
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		String string;
+//		while ((string= br.readLine())!=null) {
+//			System.out.println(string);
+//			
+//		}
+		
 		Scanner sc = new Scanner(System.in);
 		
-		int t = sc.nextInt();
-		sc.nextLine();
-		for(int i = 0 ; i <t ;i++) {
-			String string = sc.nextLine();
-			System.out.println(string.charAt(0)+""+string.charAt(string.length()-1));
+		while (sc.hasNext()) {
+			System.out.println(sc.nextLine());
 			
 		}
-		
-
 	}
 
 }
