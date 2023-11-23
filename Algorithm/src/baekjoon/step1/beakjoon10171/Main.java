8af6d77f61da3fe30ab06228bc6bc53086fe9cbf
JIHWAN <jjjh0508@gmail.com>
2023-11-23 15:33:50 +0900

    10171, 10172

---
+++ Algorithm/src/baekjoon/step1/beakjoon10171/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.step1.beakjoon10171;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
