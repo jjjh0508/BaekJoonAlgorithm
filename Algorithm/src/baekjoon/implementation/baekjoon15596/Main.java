35d917c48feaea0e8c76d15c1de9bc5933eadf17
JIHWAN <jjjh0508@gmail.com>
2023-12-16 14:16:27 +0900

    15596

---
+++ Algorithm/src/baekjoon/implementation/baekjoon15596/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.implementation.baekjoon15596;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Test test = new Test();
+		int[] a= {5,5};
+		
+		System.out.println(test.sum(a));
+		
+
+	}
+	
+
+}
