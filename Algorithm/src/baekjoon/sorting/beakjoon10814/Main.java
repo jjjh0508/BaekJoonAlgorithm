2081fd4afc59e90d7ecf312ba611f18352d9d7e9
JIHWAN <jjjh0508@gmail.com>
2024-01-08 11:12:58 +0900

    10814

---
+++ Algorithm/src/baekjoon/sorting/beakjoon10814/Main.java
@@ -0,0 +1,34 @@
+package baekjoon.sorting.beakjoon10814;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		String[][] arr = new String[n][2];
+		StringTokenizer st;
+		for(int i = 0 ;i < n ; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = st.nextToken();
+			arr[i][1] = st.nextToken();
+		}
+		
+		Arrays.sort(arr,(e1,e2)->{
+			return Integer.parseInt(e1[0])-Integer.parseInt(e2[0]);
+		});
+		
+		for(int i = 0 ; i < n ; i++) {
+			sb.append(arr[i][0]+" "+arr[i][1]).append("\n");
+		}
+		System.out.println(sb);
+
+	}
+
+}
