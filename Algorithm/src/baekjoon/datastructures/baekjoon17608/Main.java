868f7256c84aeabe29c003abf789719e31e583fd
JIHWAN <jjjh0508@gmail.com>
2024-01-27 12:06:07 +0900

    17608

---
+++ Algorithm/src/baekjoon/datastructures/baekjoon17608/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.datastructures.baekjoon17608;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for(int i = 0 ; i < n ; i++) {
+			arr[i]= Integer.parseInt(br.readLine());
+		}
+		
+		int temp = arr[n-1];
+		int count = 1;
+		for(int i = arr.length-1; i>=0 ; i--) {
+			if(temp<arr[i]) {
+				
+				count++;
+				temp=arr[i];
+			}
+		}
+		
+		System.out.println(count);
+ 
+	}
+
+}
