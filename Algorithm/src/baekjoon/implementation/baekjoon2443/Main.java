d0207f296504219c703f3e31e26dd243f5b5690a
JIHWAN <jjjh0508@gmail.com>
2024-01-28 13:47:32 +0900

    2443

---
+++ Algorithm/src/baekjoon/implementation/baekjoon2443/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.implementation.baekjoon2443;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		for(int i=0 ; i < n ;i++) {
+			for(int j = 0;j<i;j++) {
+				sb.append(" ");
+			}
+			for(int j = 0 ; j< (2*n-1)-(2*i);j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
